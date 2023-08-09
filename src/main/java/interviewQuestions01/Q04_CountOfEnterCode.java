d4ceb482a378177f1a7ad19025bef70632860a4f
ekimaycicek <ekimaycicek@icloud.com>
2023-08-09 22:27:08 +0300

    new

--- src/main/java/interviewQuestions01/Q04_CountOfEnterCode.java
+++ src/main/java/interviewQuestions01/Q04_CountOfEnterCode.java
@@ -1,7 +1,23 @@
 package interviewQuestions01;
+
+import java.util.Scanner;
+
 /*
     (ENG)   Write a code that checks your "PIN" code as a String. Block more than 3 wrong entries.
     (TR)    String olan "PIN" kodunuzu kontrol eden bir kod yazınız. 3'den fazla yanlis girislerde bloke koyunuz.
+    **Scanner oluştur.
+    **sout mesajını ver
+    **variable olustur
+    **if blogunda
  */
 public class Q04_CountOfEnterCode {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen pin kodunuzu giriniz: ");
+        String pin = input.next().toLowerCase();
+
+       
+    }
 }
