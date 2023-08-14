315be51bbcfd5fad03a0adc9fbc0b186db5c4325
ekimaycicek <ekimaycicek@icloud.com>
2023-08-14 15:41:53 +0300

    new

--- src/main/java/interviewQuestions03/Q01_Palindrome.java
+++ src/main/java/interviewQuestions03/Q01_Palindrome.java
@@ -1,4 +1,9 @@
 package interviewQuestions03;
+
+import java.util.Scanner;
+
+
+
 /*
     (ENG)Crteate a program checks if a String is PALINDROME or not.
          If a word, phrase, or sequence that reads the same backword as forward then it is called "palindrome",
@@ -9,4 +14,28 @@ package interviewQuestions03;
          Example : Ey edip Adanada pide ye,  Traş niçin şart
  */
 public class Q01_Palindrome {
-}
+
+
+        public static boolean isPalindrome(String str) {
+            StringBuilder reversedString = new StringBuilder(str);
+            reversedString.reverse();
+            return str.equals(reversedString.toString());
+        }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Bir kelime girin: ");
+            String str = scanner.nextLine();
+            if (isPalindrome(str)) {
+                System.out.println("Kelime bir palindromedur");
+            } else {
+                System.out.println("Kelime bir palindrome değildir");
+            }
+        }
+    }
+
+
+
+
+
+
