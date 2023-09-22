61056c6cf9ec8d510b0a516292e936ce278bea86
ekimaycicek <ekimaycicek@icloud.com>
2023-09-22 22:23:14 +0300

    new

--- src/main/java/interviewQuestions01/Q02_MergeCertainCharacters.java
+++ src/main/java/interviewQuestions01/Q02_MergeCertainCharacters.java
@@ -17,7 +17,7 @@ public class Q02_MergeCertainCharacters {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("please bir metin giriniz:");
+        System.out.println("please enter metin giriniz:");
         String mtn = scan.nextLine();
 
         System.out.println("Bir sayı giriniz:");
