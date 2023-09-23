e02f6b41faf795d22597cd7b56f39b972e370e2e
ekimaycicek <ekimaycicek@icloud.com>
2023-09-23 23:54:48 +0300

    new

--- src/main/java/interviewQuestions01/Q03_PrimeNumber.java
+++ src/main/java/interviewQuestions01/Q03_PrimeNumber.java
@@ -11,7 +11,7 @@ public class Q03_PrimeNumber {
 
                 Scanner scanner = new Scanner(System.in);
 
-                System.out.print("Positive enter a: ");
+                System.out.print("Positive enter a number: ");
                 int sayi = scanner.nextInt();
 
                 if (sayi <= 0) {
