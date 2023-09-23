e50e0fd84fda6c77fa923001b6c04dc28e8613f2
ekimaycicek <ekimaycicek@icloud.com>
2023-09-23 23:56:32 +0300

    new

--- src/main/java/interviewQuestions01/Q03_PrimeNumber.java
+++ src/main/java/interviewQuestions01/Q03_PrimeNumber.java
@@ -19,7 +19,7 @@ public class Q03_PrimeNumber {
                 } else if (isAsal(sayi)) {
                     System.out.println(sayi + " is a prime number.");
                 } else {
-                    System.out.println(sayi + " is not a değildir.");
+                    System.out.println(sayi + " is not a prime.");
                 }
 
                 scanner.close();
