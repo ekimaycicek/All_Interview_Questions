e0c6b01fc2cdc3a1b8da7262201434097e96fef0
ekimaycicek <ekimaycicek@icloud.com>
2023-09-24 21:47:39 +0300

    new

--- src/main/java/interviewQuestions02/Q01_ArmstrongNumber.java
+++ src/main/java/interviewQuestions02/Q01_ArmstrongNumber.java
@@ -24,12 +24,12 @@ public class Q01_ArmstrongNumber {
         int number =Math.abs(scan.nextInt());
 
         int number1 = number / 100;
-        int sayi2 = (number / 10) % 10;
+        int number2 = (number / 10) % 10;
         int sayi3 = number % 10;
 
-        double result = Math.pow(number1,3)+ Math.pow(sayi2,3) + Math.pow(sayi3,3);
+        double result = Math.pow(number1,3)+ Math.pow(number2,3) + Math.pow(sayi3,3);
 
-        if (number<100 && sayi2>999){
+        if (number<100 && number2>999){
             System.out.println("Lutfen 3 basamakli sayi giriniz");
         } else if (number==result) {
             System.out.println("Armstrong sayidir");
