d09e3d4c3b7b4af9162873f2315af01b5eb3eaab
ekimaycicek <ekimaycicek@icloud.com>
2023-09-24 21:45:58 +0300

    new

--- src/main/java/interviewQuestions02/Q01_ArmstrongNumber.java
+++ src/main/java/interviewQuestions02/Q01_ArmstrongNumber.java
@@ -20,7 +20,7 @@ public class Q01_ArmstrongNumber {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("please 3 enter bir sayi giriniz");
+        System.out.println("please 3 enter a sayi giriniz");
         int sayi =Math.abs(scan.nextInt());
 
         int sayi1 = sayi / 100;
