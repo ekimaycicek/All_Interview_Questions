a95b4457f30e4da41ed012b2cc53b4778861faea
ekimaycicek <ekimaycicek@icloud.com>
2023-09-25 22:17:14 +0300

    new

--- src/main/java/interviewQuestions02/Q03_PerfectNumber.java
+++ src/main/java/interviewQuestions02/Q03_PerfectNumber.java
@@ -34,7 +34,7 @@ public class Q03_PerfectNumber {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter ");
+        System.out.println("enter a number");
         int number = scan.nextInt();
 
         for (int i = 1; i <= number; i++) {
