dd930e0bbebd83a8216cf4614d72edf8ec1cd01b
ekimaycicek <ekimaycicek@icloud.com>
2023-09-26 21:39:41 +0300

    new

--- src/main/java/interviewQuestions02/Q04_DifferenceBetweenArray_LargestAndSmallestElements.java
+++ src/main/java/interviewQuestions02/Q04_DifferenceBetweenArray_LargestAndSmallestElements.java
@@ -38,7 +38,7 @@ public class Q04_DifferenceBetweenArray_LargestAndSmallestElements {
 
 
             int method = arrayDizi(array);
-            System.out.println("Difference: " + method);
+            System.out.println("Difference of: " + method);
         }
     }
 
