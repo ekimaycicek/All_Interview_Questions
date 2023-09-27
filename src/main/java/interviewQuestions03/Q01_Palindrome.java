cba147a65e0f2d85740b4b61e3ab95967623721f
ekimaycicek <ekimaycicek@icloud.com>
2023-09-27 22:12:25 +0300

    new

--- src/main/java/interviewQuestions03/Q01_Palindrome.java
+++ src/main/java/interviewQuestions03/Q01_Palindrome.java
@@ -24,7 +24,7 @@ public class Q01_Palindrome {
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("enter a: ");
+            System.out.println("enter a word: ");
             String str = scanner.nextLine();
             if (isPalindrome(str)) {
                 System.out.println("Kelime bir palindromedur");
