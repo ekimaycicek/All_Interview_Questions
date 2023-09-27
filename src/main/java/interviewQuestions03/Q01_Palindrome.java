0a453f88d69f08ba5aacb79453a440a4fd3ad06b
ekimaycicek <ekimaycicek@icloud.com>
2023-09-27 22:14:01 +0300

    new

--- src/main/java/interviewQuestions03/Q01_Palindrome.java
+++ src/main/java/interviewQuestions03/Q01_Palindrome.java
@@ -29,7 +29,7 @@ public class Q01_Palindrome {
             if (isPalindrome(str)) {
                 System.out.println("the word is a palindrome");
             } else {
-                System.out.println("the word");
+                System.out.println("the word is");
             }
         }
     }
