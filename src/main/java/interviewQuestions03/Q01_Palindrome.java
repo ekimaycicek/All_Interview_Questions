8f30d8f1e0adddf47a97538364576529c9da103b
ekimaycicek <ekimaycicek@icloud.com>
2023-09-27 22:13:47 +0300

    new

--- src/main/java/interviewQuestions03/Q01_Palindrome.java
+++ src/main/java/interviewQuestions03/Q01_Palindrome.java
@@ -29,7 +29,7 @@ public class Q01_Palindrome {
             if (isPalindrome(str)) {
                 System.out.println("the word is a palindrome");
             } else {
-                System.out.println("Kelime bir palindrome değildir");
+                System.out.println("the ");
             }
         }
     }
