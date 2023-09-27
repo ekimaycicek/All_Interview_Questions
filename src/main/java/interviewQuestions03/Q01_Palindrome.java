363aeb7e74345d33dd073da2ed46555882a9d54a
ekimaycicek <ekimaycicek@icloud.com>
2023-09-27 22:13:17 +0300

    new

--- src/main/java/interviewQuestions03/Q01_Palindrome.java
+++ src/main/java/interviewQuestions03/Q01_Palindrome.java
@@ -27,7 +27,7 @@ public class Q01_Palindrome {
             System.out.println("enter a word: ");
             String str = scanner.nextLine();
             if (isPalindrome(str)) {
-                System.out.println("the word");
+                System.out.println("the word is ");
             } else {
                 System.out.println("Kelime bir palindrome değildir");
             }
