e6999118a6a1b0b3b836723be0acbad695049eae
ekimaycicek <ekimaycicek@icloud.com>
2023-09-28 22:07:19 +0300

    new

--- src/main/java/interviewQuestions03/Q02_WhileCountSum.java
+++ src/main/java/interviewQuestions03/Q02_WhileCountSum.java
@@ -20,7 +20,7 @@ public class Q02_WhileCountSum {
         int sayi=0;
 
         for (int i = 1; i<=10 ; i++) {
-            System.out.println("please");
+            System.out.println("please enter ");
             int num = input.nextInt();
             adet++;
             sayi = sayi+num;
