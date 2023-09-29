53aad5634f6fd10d35fa702b1f26276b626f4c41
ekimaycicek <ekimaycicek@icloud.com>
2023-09-29 20:54:29 +0300

    new

--- src/main/java/interviewQuestions02/Q04_DifferenceBetweenArray_LargestAndSmallestElements.java
+++ src/main/java/interviewQuestions02/Q04_DifferenceBetweenArray_LargestAndSmallestElements.java
@@ -32,7 +32,7 @@ public class Q04_DifferenceBetweenArray_LargestAndSmallestElements {
 
             int[] array = new int[dizi];
             for (int i = 0; i < dizi; i++) {
-                System.out.println("of the series" + (i + 1) + "enter the . : ");
+                System.out.println("of the series" + (i + 1) + "enter the element. : ");
                 array[i] = scanner.nextInt();
             }
 
