1a2d1fb992cc63dafb42808497655d726c444a63
ekimaycicek <ekimaycicek@icloud.com>
2023-09-30 23:18:36 +0300

    new

--- src/main/java/interviewQuestions03/Q03_FrequencyOfCertainChar.java
+++ src/main/java/interviewQuestions03/Q03_FrequencyOfCertainChar.java
@@ -25,7 +25,6 @@ public class Q03_FrequencyOfCertainChar {
         for (int i = 0; i <name.length(); i++) {
             if (name.charAt(i)==ch){
                 count++;
-
             }
         }
         System.out.println("The number of repetitions: "+count);
