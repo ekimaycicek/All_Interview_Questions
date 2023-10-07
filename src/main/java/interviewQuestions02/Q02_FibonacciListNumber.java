22ef1de3be0c419491a2d8c1d9d978383c3c6240
ekimaycicek <ekimaycicek@icloud.com>
2023-10-07 23:28:56 +0300

    new

--- src/main/java/interviewQuestions02/Q02_FibonacciListNumber.java
+++ src/main/java/interviewQuestions02/Q02_FibonacciListNumber.java
@@ -26,7 +26,6 @@ public class Q02_FibonacciListNumber {
         int num = Math.abs(scan.nextInt());
 
 
-
         int fib=0;
         int fibo=1;
         System.out.print("fibonacci: "+fib+ " - "+fibo);
