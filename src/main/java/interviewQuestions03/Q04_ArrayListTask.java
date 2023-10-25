af048907f39d21c484a51c06a287b141b724c31e
ekimaycicek <ekimaycicek@icloud.com>
2023-10-25 20:31:50 +0300

    new

--- src/main/java/interviewQuestions03/Q04_ArrayListTask.java
+++ src/main/java/interviewQuestions03/Q04_ArrayListTask.java
@@ -46,7 +46,7 @@ public class Q04_ArrayListTask {
         System.out.println("Part 3 Sonucu: " + renkler);
     }
 
-    // Part 2 - İlk elemana ekleme
+    // Part 2 - İlk elemana ekl
     public static void ekleIlkElemana(ArrayList<String> liste, String eleman) {
         liste.add(0, eleman);
     }
