2bbc15d219dd61db6a8b3a2dc3617c9a11e171c5
ekimaycicek <ekimaycicek@icloud.com>
2023-10-25 20:30:57 +0300

    new

--- src/main/java/interviewQuestions03/Q04_ArrayListTask.java
+++ src/main/java/interviewQuestions03/Q04_ArrayListTask.java
@@ -36,7 +36,7 @@ public class Q04_ArrayListTask {
             System.out.println(renk);
         }
 
-        // Pa
+        // P
         String[] dizi = {"Kırmızı", "Yeşil", "Mavi", "Sarı", "Siyah"};
         ekleIlkElemana(renkler, "Turuncu");
         System.out.println("Part 2 Sonucu: " + renkler);
