533f8016d42fdf3687e953c76118c36df1b06288
ekimaycicek <ekimaycicek@icloud.com>
2023-10-25 20:31:28 +0300

    new

--- src/main/java/interviewQuestions03/Q04_ArrayListTask.java
+++ src/main/java/interviewQuestions03/Q04_ArrayListTask.java
@@ -41,7 +41,7 @@ public class Q04_ArrayListTask {
         ekleIlkElemana(renkler, "Turuncu");
         System.out.println("Part 2 Sonucu: " + renkler);
 
-        // P
+        //
         ekleSonElemanOncesine(renkler, "Pembe", "Yeşil");
         System.out.println("Part 3 Sonucu: " + renkler);
     }
