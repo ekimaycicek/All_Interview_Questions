9706a1ade8ac77d67f0e2541e76b36f4cd44f909
ekimaycicek <ekimaycicek@icloud.com>
2023-11-15 18:50:29 +0300

    new

--- src/main/java/interviewQuestions06/Q01_FrequencyOfGivenCharacters.java
+++ src/main/java/interviewQuestions06/Q01_FrequencyOfGivenCharacters.java
@@ -2,7 +2,6 @@ package interviewQuestions06;
 /*
 
 (ENG) Write a METHOD that finds and prints the frequency (repetitions) of the letters in a given text.(Interview question)
-(TR)  Verilen bir metindeki harflerin frekansını(tekrar sayısı) bulup yazdıran bir METHOD yazınız.
 
    Test data:
    Input  = AAABBCDD
