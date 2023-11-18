6f821cbb6b4509dfd4f04e29408e8258fb3b49ea
ekimaycicek <ekimaycicek@icloud.com>
2023-11-18 18:38:54 +0300

    new

--- src/main/java/interviewQuestions04/Q04_TestKeyword.java
+++ src/main/java/interviewQuestions04/Q04_TestKeyword.java
@@ -42,6 +42,15 @@ public class Q04_TestKeyword {
     }
     public static void main(String[] args) {
         char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
+        };
 
 }
 
