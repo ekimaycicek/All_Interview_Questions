79a91049c26815c67073096984d2125443f73ea5
ekimaycicek <ekimaycicek@icloud.com>
2023-11-26 22:47:12 +0300

    new

--- src/main/java/interviewQuestions05/Q05_UsingDoWhileWithCondition.java
+++ src/main/java/interviewQuestions05/Q05_UsingDoWhileWithCondition.java
@@ -15,6 +15,7 @@ public class Q05_UsingDoWhileWithCondition {
     public static void main(String[] args) {
 
 
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter an integer: ");
         int number = scanner.nextInt();
