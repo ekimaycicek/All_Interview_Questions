f6aad4851f2ff9e331781018ceabbcd826c312c9
ekimaycicek <ekimaycicek@icloud.com>
2023-11-28 22:54:29 +0300

    new

--- src/main/java/interviewQuestions03/Q03_FrequencyOfCertainChar.java
+++ src/main/java/interviewQuestions03/Q03_FrequencyOfCertainChar.java
@@ -13,6 +13,7 @@ public class Q03_FrequencyOfCertainChar {
 
 */
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a name: ");
         String name = scan.nextLine();
